76021fe2ade1d762cb7341ac41634c1cc4db9507
Sudeep Tarlekar <sudeeptarlekar@gmail.com>
2015-03-14 19:22:29 +0530

    Update regex_1.rb

--- regex/regex_1.rb
+++ regex/regex_1.rb
@@ -8,32 +8,50 @@ class RubyParser
 			exit
 		end
 	end
-
+	
 	def variableHash(fileObj)
 		varHash = Hash.new
 		varHash = {"Global"=>Array.new, "Class"=>Array.new, "Instance"=>Array.new, "Local"=>Array.new}
 		token = 0
 		for line in fileObj
-			if ( /^\s*(class).*/.match(line) || /^\s*(def)/.match(line))
-				 token = token + 1
-			elsif (/^(end)/.match(line))
+			if ( /^\s*(class).*/.match(line) || /^\s*(def)/.match(line) || /[^\w]if/.match(line))
+				token = token + 1
+			elsif (/end/.match(line))
 				token = token - 1
 			end
-			if (/^[\w|\s]+=/.match(line))
-				
-				#varHash["Local"] = varHash["Local"].to_i + 1
-			elsif (/@@[\w|\w\s]*=[\s\w|\w]+.*/.match(line) && token != 0)
-				varHash["Class"] = varHash["Class"].to_i + 1
-			elsif (/\$\w+\s*=\s*[\w|\d]+/.match(line))
-				varHash["Global"] = varHash["Global"].to_i + 1
-					
+
+			if (loc = /\w+\s*=/.match(line))
+				if matchData?(varHash["Local"], loc.to_s)
+					varHash["Local"] = varHash["Local"].push(loc.to_s)
+				end	
+			elsif (cls = /@@\w+\s*=/.match(line))
+				if matchData?(varHash["Class"], cls.to_s)
+					varHash["Class"] = varHash["Class"].push(cls)
+				end
+			elsif (glo = /\$\w+\s*=/.match(line))
+				if matchData?(varHash["Global"], glo.to_s)
+					varHash["Global"] = varHash["Global"].push(glo)
+				end
+			elsif (inst = /@\w+\s*=/.match(line))
+				if matchData?(varHash["Instance"], isnt.to_s)
+					varHash["Instance"] = varHash["Instance"].push(inst)
+				end
 			end
+			puts "#{line} #{token}"
 		end
 		puts varHash
+	end
+	
+	def matchData?(array, line)
+		for data in array
+			if data.to_s == line.to_s
+				return false
+			end
+		end
+		return true
 	end
 end
 
 puts "Enter path for ruby file"
 filePath = gets.chomp
 RubyParser.new.getVariableCount(filePath)
-
